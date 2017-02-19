3cd20c291ced36beba23e37fd3e4642724989e70
Roman Dubinin <romanonthego@gmail.com>
2017-02-19 15:12:01 +0700

    Gitignore fixed

--- webpack-configs/build/production.babel.js
+++ webpack-configs/build/production.babel.js
@@ -1,6 +1,4 @@
 import server from '../server.production.babel'
 import browser from '../browser.production.babel'
 
-// start build (not watch - it useless) dev server
-// dev server will handle prerender and browser build, no need to put it here
 export default [browser]
