4f15e8a652a1ed7beb88701f425465d6ca5df80c
Sergey Slipchenko <faergeek@gmail.com>
2017-02-19 12:23:46 +0300

    Make HMR work

--- app/browser.js
+++ app/browser.js
@@ -3,31 +3,16 @@ import 'isomorphic-fetch'
 import 'app/styles/app.styl'
 import ReactDOM from 'react-dom'
 import React from 'react'
-import {Provider} from 'react-redux'
-import {Router, browserHistory} from 'react-router'
+import {browserHistory} from 'react-router'
 import {ReduxAsyncConnect} from 'redux-connect'
 import {AppContainer} from 'react-hot-loader'
 import createStore from 'app/flux/stores'
-import routes from 'app/routes'
 import {trackPage} from 'app/utils/analytics/gaAction'
 
 const {
   store,
   history,
 } = createStore(browserHistory, window.__INITIAL_STATE__)
-
-const App = () => {
-  return (
-    <Provider store={store} key="provider">
-      <Router
-        history={history}
-        render={(props) => <ReduxAsyncConnect {...props} />}
-      >
-        {routes}
-      </Router>
-    </Provider>
-  )
-}
 
 history.listen(() => {
   const {pathname, action} = history.getCurrentLocation()
@@ -39,19 +24,33 @@ history.listen(() => {
   }
 })
 
-const render = (Component) => {
+const routerRender = (props) => <ReduxAsyncConnect {...props} />
+
+const render = () => {
+  const App = require('app/components/App')
+  const routes = require('app/routes')
+
   ReactDOM.render(
     <AppContainer>
-      <Component/>
+      <App
+        routerProps={{
+          history,
+          routes,
+          render: routerRender,
+        }}
+
+        store={store}
+      />
     </AppContainer>,
     document.getElementById('app')
   );
 }
 
-render(App)
+render()
 
 if (module.hot) {
-  module.hot.accept('./routes', () => {
-    render(App)
-  })
+  module.hot.accept([
+    'app/components/App',
+    'app/routes',
+  ], render)
 }
